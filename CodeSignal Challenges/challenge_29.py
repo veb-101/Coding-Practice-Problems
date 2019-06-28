03de587de019b8c35821f11d02d37f5b196ee372
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2019-06-28 14:30:50 +0530

    added challenge_29 (factorialTrailingZeros)

---
+++ CodeSignal Challenges/challenge_29.py
@@ -0,0 +1,37 @@
+# https://app.codesignal.com/challenge/jpaALgHRyNZCcuakt
+'''
+Given an integer n, find the number of trailing zeros in the decimal
+representation of n! (the exclamation mark means factorial).
+
+Example
+
+For n = 10, the output should be
+factorialTrailingZeros(n) = 2.
+10! = 3628800, it has 2 trailing zeros.
+'''
+
+# Matching challenge requirements 10 <= n <= 35
+
+
+def factorialTrailingZeros(n):
+    n //= 5
+    return n if n < 5 else n + 1
+
+
+def factorialTrailingZeros(n):
+    return n//4.3
+
+# works for all
+
+
+def factorialTrailingZeros(n):
+    if n < 4:
+        return 0
+    else:
+        x = n // 5
+        return x + factorialTrailingZeros(x)
+
+
+# n = > n-2 >> 2
+for i in range(10, 101):
+    print(f"{i} -> {factorialTrailingZeros(i)}")
